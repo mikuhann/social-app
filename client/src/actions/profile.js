d4c0c72aa341f4d2e2a4949e55f3e7a01a0d1da8
mikuhann <ng.kano@gmail.com>
2019-05-31 20:55:29 +0300

    add get profile action add get repos action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -6,7 +6,9 @@ import {
   GET_PROFILE,
   PROFILE_ERROR,
   UPDATE_PROFILE,
-  DELETE_ACCOUNT
+  DELETE_ACCOUNT,
+  GET_PROFILES,
+  GET_REPOS
 } from "./constants";
 
 export const getCurrentProfile = () => async dispatch => {
@@ -21,6 +23,54 @@ export const getCurrentProfile = () => async dispatch => {
       type: PROFILE_ERROR,
       payload: { msg: err.response.statusText, status: err.response.status }
     });
+  }
+};
+
+export const getProfiles = () => async dispatch => {
+  dispatch({
+    type: CLEAR_PROFILE
+  });
+  try {
+    const res = await axios.get('/api/profile');
+    dispatch({
+      type: GET_PROFILES,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+export const getProfileById = (userId) => async dispatch => {
+  try {
+    const res = await axios.get(`/api/profile/user/${userId}`);
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+export const getGitHubRepos = (gitUserName) => async dispatch => {
+  try {
+    const res = await axios.get(`/api/profile/github/${gitUserName}`);
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    })
   }
 };
 
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -3,6 +3,7 @@ import {
   PROFILE_ERROR,
   CLEAR_PROFILE,
   UPDATE_PROFILE,
+  GET_PROFILES, GET_REPOS
 } from "../actions/constants";
 
 const initialState = {
@@ -20,6 +21,18 @@ export default function (state = initialState, action) {
       return {
         ...state,
         profile: payload,
+        loading: false
+      };
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false
+      };
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
         loading: false
       };
     case PROFILE_ERROR:
