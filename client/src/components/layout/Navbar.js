9b2877799815e7f0e0aa166c9d723fd055806d4d
mikuhann <ng.kano@gmail.com>
2019-05-31 21:56:44 +0300

    add profiles component

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,6 +8,12 @@ const Navbar = ({ logout, auth: { isAuthenticated, loading } }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to='/profiles'>
+          <i className="fas fa-user-friends"/> {' '}
+          Developer profiles
+        </Link>
+      </li>
+      <li>
         <Link to='/dashboard'>
           <i className="fas fa-user"/>{' '}
           <span className='hide-sm'>Dashboard</span></Link>
@@ -22,9 +28,18 @@ const Navbar = ({ logout, auth: { isAuthenticated, loading } }) => {
   );
   const nonAuthLinks = (
   <ul>
-    <li><a href="#!">Developers</a></li>
-    <li><Link to="/register">Register</Link></li>
-    <li><Link to="/login">Login</Link></li>
+    <li>
+      <Link to='/profiles'>
+        <i className="fas fa-user-friends"/> {' '}
+        Developer profiles
+      </Link>
+    </li>
+    <li>
+      <Link to="/register">Register</Link>
+    </li>
+    <li>
+      <Link to="/login">Login</Link>
+    </li>
   </ul>
   );
   return (
