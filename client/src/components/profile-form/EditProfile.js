eafe032ddee2fde168f75214d14a95d938b81497
mikuhann <ng.kano@gmail.com>
2019-06-04 17:42:51 +0300

    fixes

--- client/src/components/profile-form/EditProfile.js
+++ client/src/components/profile-form/EditProfile.js
@@ -36,7 +36,7 @@ const EditProfile = ({profile: { profile, loading }, getCurrentProfile, createPr
       youtube: loading || !profile.social ? '' : profile.social.youtube,
       instagram: loading || !profile.social ? '' : profile.social.instagram,
     });
-  },[loading]);
+  },[loading, getCurrentProfile]);
   const {
     company,
     website,
